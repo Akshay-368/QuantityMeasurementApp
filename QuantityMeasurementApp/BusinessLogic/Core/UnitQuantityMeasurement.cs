e2f1844b8841b39493091b2b91f9266ed8c95dc1
Akshay-368 <gargakshay2004@gmail.com>
2026-02-23 12:09:34 +0530

    Added QuantityMeasurementApp under feature/UC-7Addition-With-Target-Unit-Specification with latest local copy

---
+++ QuantityMeasurementApp/BusinessLogic/Core/UnitQuantityMeasurement.cs
@@ -0,0 +1,30 @@
+namespace QuantityMeasurement.Core;
+using System;
+/// <summary>
+/// This is a class for the unit of any quantity that may arise in future .
+/// It requires a name to be set, which must be a string.
+/// </summary>
+public class Unit
+{
+    public static readonly Unit Feet = new Unit ("Feet" , 1.0 ); // Treating it as a base as per the suggestion of the UC-3
+    public static readonly Unit Inch  = new Unit("Inch", 1.0 / 12.0);
+    public static readonly Unit Yard  = new Unit("Yard", 3.0);
+    public static readonly Unit Meter = new Unit("Meter", 3.28084);
+    public static readonly Unit Centimeter = new Unit("Centimeter", 0.0328084); // Added for the UC-4
+
+    public string Name { get; }
+    // converion factor to base unit ( eg, feet )
+    public double ConversionFactorToBase { get; }
+    private Unit(string name, double conversionFactorToBase)
+    {
+        Name = name;
+        ConversionFactorToBase = conversionFactorToBase;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+
+    
+}
---
+++ QuantityMeasurementApp/BusinessLogic/Menu.cs
@@ -0,0 +1,272 @@
+namespace QuantityMeasurement.Menu;
+using QuantityMeasurement.Interfaces;
+using QuantityMeasurement.Units;
+using QuantityMeasurement.Core;
+// I Changed the name of Menu class to MenuClass as it was clashing with the name of the namespace
+public class MenuClass : IMenu
+{
+    public  void StartMenu()
+    {
+        bool running = true;
+        while (running)
+        {
+
+            
+            //  Welcome Message
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to Quantity Measurement System (Length Testing as per the UC5) ");
+            Console.ResetColor();
+
+            Console.WriteLine("\nSelect an Option:");
+            Console.WriteLine("1. Compare Lengths");
+            Console.WriteLine("2. Convert Length"); // for uc - 5
+            Console.WriteLine("3. Add Lengths ");  // for uc - 6
+            // ( For UC-6 : Returns ans in unit of first one's value , For UC-7 : you can choose unit )
+            Console.WriteLine("0. Exit");
+
+            string mainChoice = Console.ReadLine() ?? "";
+
+            switch (mainChoice)
+            {
+                case "1":
+                    HandleComparison();
+                    break;
+
+                case "2":
+                    HandleConversion();
+                    break;
+                
+                case "3":
+                    HandleAddition();   // For UC-6
+                    break;
+
+                case "0":
+                    running = false;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Invalid option selected!");
+                    Console.ResetColor();
+                    break;
+            }
+            if (running)
+            {
+                Console.WriteLine("\nPress any key to return to menu...");
+                Console.ReadKey();
+            }
+
+            
+        }
+
+
+    }
+
+    private void HandleComparison()
+    {
+        try
+        {
+            Console.WriteLine("\nChoose Unit for First Value:");
+            ShowUnitOptions();
+            string unit1Choice = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Enter first value:");
+            double value1 = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("\nChoose Unit for Second Value:");
+            ShowUnitOptions();
+            string unit2Choice = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Enter second value:");
+            double value2 = double.Parse(Console.ReadLine() ?? "0");
+
+            Length length1 = CreateLength(value1, unit1Choice);
+            Length length2 = CreateLength(value2, unit2Choice);
+
+            bool result = length1.Equals(length2);
+
+            ShowResult($"{value1} {length1.unit} == {value2} {length2.unit}", result);
+        }
+        catch (Exception)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Error: Invalid input!");
+            Console.ResetColor();
+        }
+    }
+
+    private static void ShowUnitOptions()
+    {
+        Console.WriteLine("1. Feet");
+        Console.WriteLine("2. Inches");
+        Console.WriteLine("3. Yard");
+        Console.WriteLine("4. Meter");
+        Console.WriteLine("5. Centimeter");
+    }
+
+
+    private static void ShowResult(string comparison, bool isTrue)
+    {
+        Console.ForegroundColor = isTrue ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.WriteLine($"\nResult: {comparison} is {isTrue.ToString().ToUpper()}");
+        Console.ResetColor();
+    }
+
+        /// <summary>
+        /// Creates a new Length object based on the value and unit choice
+        /// </summary>
+        /// <param name="value">The value of the length</param>
+        /// <param name="unitChoice">The unit of the length (1 for Feet, 2 for Inches, 3 for Yard, 4 for Meter)</param>
+        /// <returns>A new Length object</returns>
+        /// <exception cref="Exception">Thrown if the unit choice is invalid</exception>
+    private static Length CreateLength(double value, string unitChoice)
+    {
+        return unitChoice switch
+        {
+            "1" => new Feet(value),
+            "2" => new Inches(value),
+            "3" => new Yard(value),
+            "4" => new Meter(value),
+            "5" => new Centimeter(value),
+            _ => throw new Exception("Invalid Unit")
+        };
+    }
+
+    private void HandleConversion()
+    {
+        Console.WriteLine("Choose Unit:");
+        ShowUnitOptions();
+
+        string fromChoice = Console.ReadLine() ?? "";
+
+        Console.WriteLine("Enter value:");
+        double value = double.Parse(Console.ReadLine() ?? "0");
+
+        Console.WriteLine("Convert To:");
+        ShowUnitOptions();
+
+        string toChoice = Console.ReadLine() ?? "";
+
+        Length original = CreateLength(value, fromChoice);
+        Unit targetUnit = GetUnitFromChoice(toChoice);
+
+        Length converted = original.ConvertTo(targetUnit);
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"\nConverted Value: {converted}");
+        Console.ResetColor();
+    }
+
+    private static Unit GetUnitFromChoice(string choice)
+    {
+        return choice switch
+        {
+            "1" => Unit.Feet,
+            "2" => Unit.Inch,
+            "3" => Unit.Yard,
+            "4" => Unit.Meter,
+            "5" => Unit.Centimeter,
+            _ => throw new Exception("Invalid Unit")
+        };
+    }
+
+    // For UC-6
+
+    /* private void HandleAddition()
+    {
+        try
+        {
+            Console.WriteLine("\nChoose Unit for First Value:");
+            ShowUnitOptions();
+            string unit1Choice = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Enter first value:");
+            double value1 = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("\nChoose Unit for Second Value:");
+            ShowUnitOptions();
+            string unit2Choice = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Enter second value:");
+            double value2 = double.Parse(Console.ReadLine() ?? "0");
+
+            Length length1 = CreateLength(value1, unit1Choice);
+            Length length2 = CreateLength(value2, unit2Choice);
+
+            Length result = length1.Add(length2);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nResult: {length1} + {length2} = {result}");
+            Console.ResetColor();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Error: {ex.Message}");
+            Console.ResetColor();
+        }
+    } */
+
+
+    // for uc- 6 and uc-7 
+    private void HandleAddition()
+    {
+        try
+        {
+            Console.WriteLine("\nChoose Unit for First Value:");
+            ShowUnitOptions();
+            string unit1Choice = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Enter first value:");
+            double value1 = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("\nChoose Unit for Second Value:");
+            ShowUnitOptions();
+            string unit2Choice = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Enter second value:");
+            double value2 = double.Parse(Console.ReadLine() ?? "0");
+
+            Length length1 = CreateLength(value1, unit1Choice);
+            Length length2 = CreateLength(value2, unit2Choice);
+
+            Console.WriteLine("\nSelect Addition Mode:");
+            Console.WriteLine("1. Add (Result in First Unit)  [UC6]");
+            Console.WriteLine("2. Add and Choose Target Unit [UC7]");
+
+            string modeChoice = Console.ReadLine() ?? "";
+
+            Length result;
+
+            if (modeChoice == "1")
+            {
+                result = length1.Add(length2);
+            }
+            else if (modeChoice == "2")
+            {
+                Console.WriteLine("\nChoose Target Unit:");
+                ShowUnitOptions();
+                string targetChoice = Console.ReadLine() ?? "";
+
+                Unit targetUnit = GetUnitFromChoice(targetChoice);
+
+                result = length1.Add(length2, targetUnit);
+            }
+            else
+            {
+                throw new Exception("Invalid Addition Mode");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nResult: {length1} + {length2} = {result}");
+            Console.ResetColor();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Error: {ex.Message}");
+            Console.ResetColor();
+        }
+    }
+
+}
